4285c21c8e26a6f7665fde5f3451ac20526c0e1c
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-04 17:16:37 +0530

    CRUD operations done

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,40 @@ app.use(express.json());
 // call back function to handle the root route
 app.get("/", (req, res) => {
   res.status(200).send("Hello, World!!!");
-  //   console.log(`Response status: ${res.status}`);
 });
 
-// adding route for the books model
+// get all books and print the count of the books
 app.get("/books", async (req, res) => {
   try {
     const books = await Book.find();
-    res.status(200).json(books);
+    res.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.error("Error fetching books:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// find book by specific criteria
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.error("Error fetching book:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// create a new book
 app.post("/books", async (req, res) => {
   try {
     const { title, author, publisher, publishingYear } = req.body;
@@ -36,10 +56,49 @@ app.post("/books", async (req, res) => {
     await newBook.save();
     res.status(201).json({
       message: "Book created successfully",
-      book: newBook
+      book: newBook,
     });
   } catch (error) {
     console.error("Error creating book:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, publisher, publishingYear } = req.body;
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      { title, author, publisher, publishingYear },
+      { new: true, runValidators: true }
+    );  
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+    res.status(200).json({
+      message: "Book updated successfully",
+      book: updatedBook,
+    });
+  } catch (error) {
+    console.error("Error updating book:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book not found." });
+    }
+    res.status(200).json({
+      message: "Book deleted successfully",
+      book: deletedBook,        
+    });
+  } catch (error) {
+    console.error("Error deleting book:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
